5c2463b05f2320f6d8abfab12d2602e235b0898c
justin <jebusiness723@gmail.com>
2022-01-23 02:15:34 -0800

    ACF - saving field groups

--- app/admin.php
+++ app/admin.php
@@ -22,3 +22,8 @@ add_action('customize_register', function (\WP_Customize_Manager $wp_customize) 
 add_action('customize_preview_init', function () {
     wp_enqueue_script('sage/customizer.js', asset_path('scripts/customizer.js'), ['customize-preview'], null, true);
 });
+
+/**
+ * ACF
+ */
+require_once( get_template_directory() . '/../app/admin/acf.php' );
--- resources/views/template-home.blade.php
+++ resources/views/template-home.blade.php
@@ -7,6 +7,19 @@
 @section('content')
   @while(have_posts()) @php the_post() @endphp
     @include('modules.template-home.hero')
-    @include('partials.content-page')
+    @if ( have_rows('content') )
+      @php
+      $layout_key = 0;
+      @endphp
+      @while ( have_rows('content') ) @php the_row() @endphp
+        @php
+        $layout = str_replace('_', '-', get_row_layout());
+        @endphp
+        @include('flexible.' . $layout, ['layout_key' => $layout_key])
+        @php
+        $layout_key++;
+        @endphp
+      @endwhile
+    @endif    
   @endwhile
 @endsection
