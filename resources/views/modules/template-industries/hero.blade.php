a908f5a78920cd1ade4146f0c12096e37340a0e6
justin <jebusiness723@gmail.com>
2022-02-10 18:39:38 -0800

    Industries - Hero

--- resources/views/modules/template-industries/hero.blade.php
+++ resources/views/modules/template-industries/hero.blade.php
@@ -4,8 +4,9 @@ $overlay = get_field('overlay');
 $color = get_field('color');
 $text_alignment = get_field('text_alignment');
 @endphp
-<section class="flex-layout py-5 text-{{ $text_alignment }}" {!! $overlay['enable'] ? 'style="background-color:'.($overlay['background_color']).'; opacity:'.$overlay['opacity'].';"' : '' !!}>
-    <div class="container" style="color: {{ $color }}">
-        <h1 class="hero_inner_heading m-0">{{ $subheading }}</h1>
-    </div>
+<section class="flex-layout py-5 text-{{ $text_alignment }}" {!! $overlay['enable'] ? 'style="background-color: ' . $overlay['background_color'] . '; opacity: ' . $overlay['opacity'] . ';"' : '' !!}>
+  <div class="container" style="color: {{ $color }}">
+    <h1 class="d-none">{!! get_the_title() !!}</h1>
+    <h3 class="hero_inner_heading m-0">{!! $subheading !!}</h3>
+  </div>
 </section>
