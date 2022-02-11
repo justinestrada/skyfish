6e7e5c4b0564fbd26f750541b5b27d372f3ee634
justin <jebusiness723@gmail.com>
2022-02-10 23:51:43 -0800

    Flex - One Column - Heading alignment

--- resources/views/flexible/one-column.blade.php
+++ resources/views/flexible/one-column.blade.php
@@ -1,15 +1,14 @@
 @php
 $section_padding = get_sub_field('padding');
 $background = get_sub_field('background');
-$text = get_sub_field('color');
-$text_alignment = get_sub_field('text_alignment');
+$text = get_sub_field('text');
 @endphp
 <section id="flex-layout_{{ $layout_key }}" class="flex-layout flex-layout_multi-column-card-icon-heading-content bg-{{ $background['color'] }} {{ $section_padding }} text-{{ $text['color'] }} text-{{ $text['alignment'] }}">
   <div class="container">
     <div class="row">
-      <div class="col">
+      <div class="col text-{{ $text['alignment'] }}">
         @if ($heading = get_sub_field('heading'))
-          <h2 class="flex-layout_heading text-{{ $text['heading'] }} text-{{ $text['alignment'] }} mb-3">{!! $heading !!}</h2>
+          <h2 class="flex-layout_heading text-{{ $text['heading']['mobile_alignment'] }} text-lg-{{ $text['heading']['desktop_alignment'] }} mb-3">{!! $heading !!}</h2>
         @endif
         @if ($content = get_sub_field('content'))
           <div class="flex-layout_inner-content">{!! $content !!}</div>
