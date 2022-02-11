df9a4da905c58dc6822cf74494f520fb8bcc785f
justin <jebusiness723@gmail.com>
2022-02-11 00:48:17 -0800

    Flex - full width image

--- resources/views/flexible/full-width-image.blade.php
+++ resources/views/flexible/full-width-image.blade.php
@@ -6,7 +6,7 @@
   @php ($parallax = get_sub_field('parallax'))
   <section id="flex-layout_{{ $layout_key }}" class="flex-layout flex-layout_full-width-image {{  isset($parallax) && $parallax['enable'] ? 'flex-layout_full-width-image_parallax ' . $parallax['padding'] : ''  }}" style="background-image: url('{{ $image['url'] }}');">
     <img src="{{ $image['url'] }}" alt="{{ $image['alt'] }}" class="w-100 {{ isset($parallax) && $parallax['enable'] ? 'd-lg-none' : '' }}"/>
-    @if ($parallax && $parallax['padding'] !== 'viewport_height')
+    @if ($parallax && $parallax['enable'] && $parallax['padding'] !== 'viewport_height')
       <div class="d-none d-lg-block my-lg-10">
         <span class="sr-only">{{ $image['alt'] }}</span>
       </div>
