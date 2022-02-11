15345b17cbb938e994d23fbb4fcec7e8b393afaf
Abdul Rehman <abdulrehmanali82@gmail.com>
2022-02-11 05:02:15 +0500

    Removed padding and background

--- resources/views/flexible/full-width-image.blade.php
+++ resources/views/flexible/full-width-image.blade.php
@@ -1,8 +1,4 @@
-@php
-$background = get_sub_field('background');
-$section_padding = get_sub_field('padding');
-@endphp
-<section id="flex-layout_{{ $layout_key }}" class="flex-layout flex-layout_multi-column-card-icon-heading-content bg-{{ $background['color'] }} {{ $section_padding }} text-{{ $text_color }}">
+<section id="flex-layout_{{ $layout_key }}" class="flex-layout flex-layout_multi-column-card-icon-heading-content">
   @if ($image = get_sub_field('image'))
     <img src="{{ $image }}" class="w-100"/>
   @endif
