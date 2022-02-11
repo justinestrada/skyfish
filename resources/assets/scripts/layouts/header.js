ed320b64a3fac819a497796f0a6681974b612985
Abdul Rehman <abdulrehmanali82@gmail.com>
2022-02-11 23:44:10 +0500

    Right Slide Header (#9)
    * Right Slide Header
    
    * Header - Mobile menu - scss
    
    * Header - off canvas menu - js
    
    Co-authored-by: justin <jebusiness723@gmail.com>

--- resources/assets/scripts/layouts/header.js
+++ resources/assets/scripts/layouts/header.js
@@ -1,6 +1,17 @@
 
 export const Header = {
   onLoad: function() {
+    this.onScroll()
+    this.onBodyClick()
+  },
+  onBodyClick: function() {
+    $(document.body).on('click', function(e) {
+      if ($(e.target).closest('.navbar-collapse').length || jQuery(e.target).closest('.navbar-toggler').length) return // Not return false
+      $('.navbar-collapse').removeClass('show')
+      $('.navbar-toggler').addClass('collapsed')
+    })
+  },
+  onScroll: function() {
     $(window).on('scroll', function() {
       if($(this).scrollTop() > 16) {
         $('header#main-nav > nav').addClass('z-depth-1')
