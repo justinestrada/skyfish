ee49a6bbe32233d9b08cb46f3234b4bcf522bd65
Abdul Rehman <abdulrehmanali82@gmail.com>
2022-02-11 18:20:33 +0500

    Right Slide Header

--- resources/assets/scripts/layouts/header.js
+++ resources/assets/scripts/layouts/header.js
@@ -1,6 +1,18 @@
 
 export const Header = {
   onLoad: function() {
+    this.onScroll();
+    this.onBodyClick();
+  },
+  onBodyClick: function() {
+    jQuery(document.body).on('click', function(ev){
+      if(jQuery(ev.target).closest('.navbar-collapse').length || jQuery(ev.target).closest('.navbar-toggler').length) return; // Not return false
+      jQuery('.navbar-collapse').removeClass('show');
+      jQuery('.navbar-toggler').addClass('collapsed');
+    });
+  },
+
+  onScroll: function() {
     $(window).on('scroll', function() {
       if($(this).scrollTop() > 16) {
         $('header#main-nav > nav').addClass('z-depth-1')
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,40 +1,41 @@
-
 <style>
-header > nav .main-nav-menu-items_container .main-nav-menu-items .sub-menu .menu-item a img {
-  margin-bottom: 1rem;
-}
-@media (min-width: 992px) {
-  header > nav .main-nav-menu-items {
-    column-gap: 1rem;
-  }
-}
+    header>nav .main-nav-menu-items_container .main-nav-menu-items .sub-menu .menu-item a img {
+        margin-bottom: 1rem;
+    }
+
+    @media (min-width: 992px) {
+        header>nav .main-nav-menu-items {
+            column-gap: 1rem;
+        }
+    }
+
 </style>
-<header id="main-nav">
-  <nav class="navbar navbar-expand-lg fixed-top bg-secondary">
-    <div class="container-xxl">
-      <a class="navbar-brand" href="{{ home_url('/') }}">
-        <img src="{{ App\asset_path('images/logo.svg') }}" alt="{{ get_bloginfo('name', 'display') }}" style="height: 40px;"/>
-        <span class="sr-only">{{ get_bloginfo('name', 'display') }}</span>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <div id="main-nav-icon" className="hamburger-close-animated-icon">
-          <span></span>
-          <span></span>
-          <span></span>
+<header>
+    <nav class="navbar navbar-expand-lg fixed-top bg-secondary">
+        <div class="container-xxl">
+            <a class="navbar-brand" href="{{ home_url('/') }}">
+                <img src="{{ App\asset_path('images/logo.svg') }}" alt="{{ get_bloginfo('name', 'display') }}"
+                    style="height: 40px;" />
+                <span class="sr-only">{{ get_bloginfo('name', 'display') }}</span>
+            </a>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdown">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                @if (has_nav_menu('primary_navigation'))
+                    {!! wp_nav_menu([
+                          'theme_location' => 'primary_navigation',
+                          'menu_class' => 'main-nav-menu-items',
+                          'container_class' => 'main-nav-menu-items_container',
+                      ]) !!}
+                @endif
+                <div class="d-none d-lg-block">
+                    <img src="{{ App\asset_path('images/american-made.png') }}" alt="American Made"
+                        style="height: 32px;" />
+                </div>
+            </div>
         </div>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        @if (has_nav_menu('primary_navigation'))
-          {!! wp_nav_menu([
-            'theme_location' => 'primary_navigation',
-            'menu_class' => 'main-nav-menu-items',
-            'container_class' => 'main-nav-menu-items_container'
-          ]) !!}
-        @endif
-        <div class="d-none d-lg-block">
-          <img src="{{ App\asset_path('images/american-made.png') }}" alt="American Made" style="height: 32px;"/>
-        </div>
-      </div>
-    </div>
-  </nav>
+    </nav>
 </header>
