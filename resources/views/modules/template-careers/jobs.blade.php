4c1aca539e8bbecc313a71ea966c41983ad39896
justin <jebusiness723@gmail.com>
2022-02-13 00:42:06 -0800

    Careers

--- resources/views/modules/template-careers/jobs.blade.php
+++ resources/views/modules/template-careers/jobs.blade.php
@@ -1,5 +1,8 @@
 
 <style>
+.badge {
+  border-radius: 0;
+}
 .card-job:hover::before {
   opacity: 1;
 }
@@ -14,6 +17,15 @@
   opacity: 0;
   transition: opacity 0.3s ease-in-out 0s;
 }
+.card-job .badge {
+  position: absolute;
+  top: 1.25rem;
+  right: 1.25rem;
+}
+.badge-new {
+  background-color: #f44518;
+  color: white;
+}
 </style>
 <section id="jobs">
   <h3 class="d-none">Jobs Openings</h3>
@@ -23,6 +35,12 @@
         <div class="col-lg-6 mb-5">
           <a href="{{ get_permalink($job->ID) }}" class="card card-job bg-secondary text-white">
             <div class="card-body">
+              @php
+              $less_month_ago = time() < strtotime($job->post_date . ' +1 month');
+              @endphp
+              @if ($less_month_ago)
+                <span class="badge badge-new">New</span>
+              @endif
               <div class="row">
                 <div class="col-8">
                   <h4 class="mb-2">{{ $job->post_title }}</h4>
@@ -30,15 +48,15 @@
                   $metadata = get_field('metadata', $job->ID);
                   @endphp
                   @foreach ($metadata as $meta)
-                    <div>{{ $meta['value'] }}</div>
+                    <div class="text-gray">{{ $meta['value'] }}</div>
                   @endforeach
                 </div>
-                <div class="col-4 d-flex align-items-center">
+                <div class="col-4 d-flex align-items-center justify-content-end">
                   @php
                   $date_stamp = strtotime($job->post_date);
                   $post_date = date('F jS, Y', $date_stamp);
-                  @endphp               
-                  {{ $post_date }}
+                  @endphp
+                  <small>{{ $post_date }}</small>
                 </div>  
               </div>
             </div>
