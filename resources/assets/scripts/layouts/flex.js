8e172c06dca3a815b255e22b1d8872209ce931fa
Justin Estrada <6895474+justinestrada@users.noreply.github.com>
2022-02-17 00:42:38 -0800

    Update flex.js

--- resources/assets/scripts/layouts/flex.js
+++ resources/assets/scripts/layouts/flex.js
@@ -7,27 +7,26 @@ export const Flex = {
     if (!$('.avatar-cards').length) {
       return
     }
-    console.log('s');
-    $('.avatar-cards .card-content').map((k,element) => {
+    $('.avatar-cards .card-content').map((k, element) => {
       const length = element.innerHTML.length;
       const height = ($(element).height() + 50);
       if (length < 256) {
-        $(element).closest('.load-more-content').hide();
-        return;
+        $(element).closest('.load-more-content').hide()
+        return
       }
       $(element).addClass('truncate')
-      const load_more_content = $(element).parent().find('.load-more-content');
+      const load_more_content = $(element).parent().find('.load-more-content')
       load_more_content.on('click', function(){
         if ($(element).hasClass('active')) {
-          $(element).animate({height: '100px'}).removeClass('active');
-          load_more_content.addClass('fa-angle-double-down');
-          load_more_content.removeClass('fa-angle-double-up');
+          $(element).animate({height: '100px'}).removeClass('active')
+          load_more_content.addClass('fa-angle-double-down')
+          load_more_content.removeClass('fa-angle-double-up')
         } else {
-          $(element).animate({height: height+'px'}).addClass('active');
-          load_more_content.removeClass('fa-angle-double-down');
-          load_more_content.addClass('fa-angle-double-up');
+          $(element).animate({height: height+'px'}).addClass('active')
+          load_more_content.removeClass('fa-angle-double-down')
+          load_more_content.addClass('fa-angle-double-up')
         }
-      });
-    });
+      })
+    })
   },
 }
