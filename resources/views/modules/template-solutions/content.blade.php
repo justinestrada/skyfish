fc4957b3195c0e93c1f5303de9febc12b92c7ca8
justin <jebusiness723@gmail.com>
2022-02-22 21:14:38 -0800

    Solutions

--- resources/views/modules/template-solutions/content.blade.php
+++ resources/views/modules/template-solutions/content.blade.php
@@ -7,8 +7,10 @@
           <div class="pillars_bg bg-{{ $solution['hover_background']['color'] }}" key="{{ $key }}" style="background-image: url('{{ $solution['hover_background']['image']['url'] }}');"></div>
         @endforeach
         @foreach ($solutions as $key => $solution)
-          <a href="{{ get_permalink($solution['page']->ID) }}" class="pillar col-lg-3 text-center py-5" style="background-image: url('{{ $solution['icon']['url'] }}');"
+          <a href="{{ get_permalink($solution['page']->ID) }}" class="pillar col-lg-3 text-center py-5"
+            style="background-image: url('{{ $solution['background_image']['url'] }}');"
             key="{{ $key }}">
+            <div class="pillar_inner-bg bg-fade-up"></div>
             <div class="pillar_text">
               <h3 class="text-white mb-2">{!! $solution['title'] !!}</h3>
               <div class="pillar_text_description text-white">
