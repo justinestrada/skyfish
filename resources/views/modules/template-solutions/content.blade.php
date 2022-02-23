28fa278092d6917f482eae18362a2114fcada2d9
justin <jebusiness723@gmail.com>
2022-02-22 18:30:46 -0800

    Solutions - Pillars

--- resources/views/modules/template-solutions/content.blade.php
+++ resources/views/modules/template-solutions/content.blade.php
@@ -1,7 +1,23 @@
 
 <style>
-.pillars:hover {
+.pillars {
+  position: relative;
+}
+.pillars_bg {
+  display: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+}
+/* .pillars:hover {
   background-color: pink !important;
+} */
+a.pillar:hover {
+  text-decoration: none;
 }
 .pillars .pillar {
   display: flex;
@@ -14,6 +30,10 @@
   background-repeat: no-repeat;
   background-position: center;  
 }
+.pillars:hover .pillar {
+  background-color: transparent;
+  /* background-image: none !important; */
+}
 .pillars .pillar:last-child {
   border-right: none;
 }
@@ -23,18 +43,10 @@
   align-content: center;
   flex-direction: column;
 }
-.pillar .pillar_text_description {
+.pillar .pillar_text_description.pillar_text_description_initialized {
   height: 0;
   overflow: hidden;
-  transition: all 1s;
-}
-.pillar:hover {
-  background-color: transparent;
-  background-image: none !important;
-}
-.pillar:hover .pillar_text_description {
-  height: fit-content;
-  /* background-color: blue; */
+  transition: height 500ms;
 }
 </style>
 <section id="solutions" class="py-10">
@@ -43,11 +55,16 @@
     <div class="pillars bg-black row">
       @if ($solutions = get_field('solutions'))
         @foreach ($solutions as $key => $solution)
-          <a href="{{ get_permalink($solution['page']->ID) }}" class="pillar col-lg-3 text-center py-5" style="background-image: url('{{ $solution['icon']['url'] }}');">
-            {{-- <img src="" alt="{{ $solution['icon']['alt'] }}" class="mb-3" style="height: 64px;"/> --}}
+          <div class="pillars_bg bg-{{ $solution['hover_background']['color'] }}" key="{{ $key }}" style="background-image: url('{{ $solution['hover_background']['image']['url'] }}');"></div>
+        @endforeach
+        @foreach ($solutions as $key => $solution)
+          <a href="{{ get_permalink($solution['page']->ID) }}" class="pillar col-lg-3 text-center py-5" style="background-image: url('{{ $solution['icon']['url'] }}');"
+            key="{{ $key }}">
             <div class="pillar_text">
-              <h3 class="text-white mb-0">{!! $solution['title'] !!}</h3>
-              <div class="pillar_text_description">{!! $solution['description'] !!}</div>
+              <h3 class="text-white mb-2">{!! $solution['title'] !!}</h3>
+              <div class="pillar_text_description text-white">
+                {!! $solution['description'] !!}
+              </div>
             </div>
           </a>
         @endforeach
@@ -63,11 +80,29 @@ const Solutions = {
       return
     }
     this.onMouseOverPillar()
+    $('.pillar_text_description').each(function() {
+      const original_height = $(this).height()
+      $(this).attr('original-height', original_height)
+      $(this).addClass('pillar_text_description_initialized')
+    })
   },
   onMouseOverPillar: function() {
-    $('.pillars').mouseover(function() {
-      const hover_background_image = $(this).attr('hover-background-image')
-      console.log('this', hover_background_image)
+    $('.pillars').each(function() {
+      const $pillars = $(this)
+      const $pillar_bg = $pillars.find('.pillars_bg')
+      $pillars.find('.pillar').mouseover(function() {
+        const $pillar = $(this)
+        const key = $pillar.attr('key')
+        const original_height = $pillar.find('.pillar_text_description').attr('original-height')
+        $pillar.find('.pillar_text_description').css('height', original_height + 'px')
+        $pillars.find(`.pillars_bg[key="${key}"]`).fadeIn(500)
+      })
+      $pillars.find('.pillar').mouseleave(function() {
+        const $pillar = $(this)
+        const key = $pillar.attr('key')
+        $pillar.find('.pillar_text_description').css('height', '0')
+        $pillars.find(`.pillars_bg[key="${key}"]`).fadeOut()
+      })
     })
   },
 }
