228b3ae1a93fb8943d61344fdd1842fededadb42
Abdul Rehman <abdulrehmanali82@gmail.com>
2022-02-26 04:09:09 +0500

    Navbar improvements

--- resources/assets/scripts/layouts/header.js
+++ resources/assets/scripts/layouts/header.js
@@ -3,6 +3,12 @@ export const Header = {
   onLoad: function() {
     this.onScroll()
     this.onBodyClick()
+    this.subMenuWidth()
+    this.makeMenuResponsive()
+    $(window).on('resize', ()=>{
+      this.subMenuWidth()
+      this.makeMenuResponsive()
+    })
   },
   onBodyClick: function() {
     $(document.body).on('click', function(e) {
@@ -18,6 +24,52 @@ export const Header = {
       } else {
         $('header > nav').removeClass('z-depth-1')
       }
+    })
+  },
+  subMenuWidth: function() {
+    const menu_menu = $('#menu-menu')
+    if (!menu_menu.length) {
+      return
+    }
+    $('> li', menu_menu).map((i, e)=>{
+      let sub_menu = $('ul.sub-menu', e);
+      if (!sub_menu) {
+        return;
+      }
+      if($(window).width() > 991) {
+        sub_menu.css('width', Math.round(menu_menu.width())+'px')
+        sub_menu.css('left', Math.round(menu_menu.position().left)+'px')
+      } else {
+        sub_menu.removeAttr('style')
+      }
+    })
+  },
+  makeMenuResponsive: function() {
+    const menu_menu = $('#menu-menu')
+    if (!menu_menu.length) {
+      return
+    }
+    if($(window).width() > 991) { 
+      return;
+    }
+    $('> li', menu_menu).map((i, e)=>{
+      let sub_menu = $('ul.sub-menu', e);
+      if (!sub_menu) {
+        return;
+      }
+      $(e).off('click')
+      $(e).on('click', (itemClick)=>{
+        let itemCicked = $(itemClick.currentTarget); 
+        itemClick.preventDefault()
+        $('> li', menu_menu).map((_, siblings)=>{
+          if($(siblings).hasClass('active_slide') && itemCicked.attr('id') != $(siblings).attr('id')) {
+            $('ul.sub-menu', siblings).slideUp()
+            $(siblings).removeClass('active_slide')
+          }
+        })
+        sub_menu.slideToggle()
+        itemCicked.toggleClass('active_slide')
+      })
     })
   },
 }
