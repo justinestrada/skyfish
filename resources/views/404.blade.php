ef53ab6c0ebd8480faf7478ac5b9fee730ab0d16
Abdul Rehman <abdulrehmanali82@gmail.com>
2022-03-02 03:28:13 +0500

    404 Page (#15)
    * 404 Page
    
    * Removed package-lock.json

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,18 +1,27 @@
 @extends('layouts.app')
+@php
+  $hero = get_field('404_background', 'option');
+  $padding = get_field('404_padding', 'option');
+@endphp
 
 @section('content')
-  <div id="content" class="py-5">
-    @include('partials.page-header')
-    @if (!have_posts())
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <div class="alert alert-warning mb-0">
-              {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-            </div>
+  <section id="hero_404"
+      class="page-hero flex-layout flex-layout_hero bg-{{ $hero['color'] }} {{ $padding }}"
+      {!! isset($hero['image']) && $hero['image'] ? 'style="background-image: url(' . $hero['image']['url'] . '); background-size: cover; background-position: center;"' : '' !!}>
+      @if ($hero['overlay'])
+          <div class="flex-layout_overlay"
+              style="background-color: {{ $hero['overlay']['color'] }}; opacity: {{ $hero['overlay']['opacity'] }};">
           </div>
-        </div>
+      @endif
+      <div class="container-xxl">
+          <div class="row">
+              <div class="col text-{{ $hero['color'] }}">
+                  <h1 class="mb-3">-404</h1>
+                  <h2 class="mb-3">Houston, <br/>We have a problem.</h2>
+                  <p id="hero_404_subheading" class="mb-3">It seems that you're stuck in tree. <br/> Let us help you get out and back in your owner arms.</p>
+                  <a href="{{ get_site_url() }}" class="btn action-btn">Take me home</a>
+              </div>
+          </div>
       </div>
-    @endif
-  </div>
+  </section>
 @endsection
