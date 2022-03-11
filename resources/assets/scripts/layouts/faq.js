d9f51655cbd97da15b39c4bc7d8120d7195a85d8
Justin Estrada <6895474+justinestrada@users.noreply.github.com>
2022-03-10 20:57:17 -0800

    Update faq.js

--- resources/assets/scripts/layouts/faq.js
+++ resources/assets/scripts/layouts/faq.js
@@ -4,35 +4,35 @@ export const FAQ = {
     this.faqOnSearch()
   },
   categoryClick: function() {
-    let faq_categories = $('#faq_categories');
-    if (!faq_categories.length) {
+    const $faq_categories = $('#faq_categories')
+    if (!$faq_categories.length) {
       return
     }
-    let cards = $('.card', faq_categories)
-    cards.on('click', function () {
-      cards.removeClass('active')
+    const $cards = $('.card', $faq_categories)
+    $cards.on('click', function () {
+      $cards.removeClass('active')
       $('#qaAccordion').fadeIn()
       $(this).addClass('active')
-      let category = $(this).attr('data-category')
+      const category = $(this).attr('data-category')
       let founded = 0;
-      $('#qaAccordion .accordion-item').map((i,e)=>{
+      $('#qaAccordion .accordion-item').map((i, e) => {
         if ($(e).attr('data-category') == category) {
           $(e).slideUp()
         } else {
-          founded++;
+          founded++
           $(e).slideDown()
         }
-      });
-      $('#questions_available').html(founded+ ' Question(s)');
+      })
+      $('#questions_available').html(`${founded} Question(s)`)
     });
   },
   faqOnSearch: function() {
-    let faq_searchbar = $('#faq_searchbar');
-    if (!faq_searchbar.length) {
+    const $faq_searchbar = $('#faq_searchbar')
+    if (!$faq_searchbar.length) {
       return
     }
-    faq_searchbar.on('keyup', function(){
-      let search = $(this).val();
+    $faq_searchbar.on('keyup', function(){
+      const search = $(this).val();
       if (!search) {
         $('#qaAccordion').slideUp()
         $('#faq_categories').slideDown()
@@ -40,16 +40,16 @@ export const FAQ = {
       }
       $('#faq_categories').slideUp()
       $('#qaAccordion').slideDown()
-      let founded = 0;
-      $('#qaAccordion .accordion-item').map((i,e)=>{
+      let founded = 0
+      $('#qaAccordion .accordion-item').map((i, e) => {
         if ($(e).attr('data-question').includes(search) || $(e).attr('data-answer').includes(search) || $(e).attr('data-category').includes(search) ) {
           $(e).slideDown()
-          founded++;
+          founded++
         } else {
           $(e).slideUp()
         }
-      });
-      $('#questions_available').html(founded+ ' Question(s)');
+      })
+      $('#questions_available').html(`${founded} Question(s)`)
     })
   },
 }
