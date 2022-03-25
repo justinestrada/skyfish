778cafd49b77d123bd942018718ea5b04fffa5e9
Abdul Rehman <abdulrehmanali82@gmail.com>
2022-03-26 03:59:40 +0500

    Job apply form

--- resources/assets/scripts/modules/gf_form_group.js
+++ resources/assets/scripts/modules/gf_form_group.js
@@ -1,6 +1,6 @@
 export const GF_Form_Group = {
   onLoad: function() {
-    if (!$('.skyfish-gf-form-group').length) {
+    if (!$('.gfield').length) {
       return;
     }
     setTimeout(function() {
@@ -10,23 +10,23 @@ export const GF_Form_Group = {
     this.onBlur();
   },
   init: function() {
-    $('.skyfish-gf-form-group input, .skyfish-gf-form-group textarea').each(function() {
+    $('.gfield input, .gfield textarea').each(function() {
       const $form_group = $(this).parent().parent();
-      if ( $(this).val() !== '' ) {
+      if ( $(this).val() !== '' || $(this).attr('type') === 'file') {
         $form_group.addClass('is-focused');
       }
     });
   },
   onFocus: function() {
-    $('.skyfish-gf-form-group input, .skyfish-gf-form-group textarea').on('focus', function() {
+    $('.gfield input, .gfield textarea').on('focus', function() {
       const $form_group = $(this).parent().parent();
       $form_group.addClass('is-focused');
     });
   },
   onBlur: function() {
-    $('.skyfish-gf-form-group input, .skyfish-gf-form-group textarea').on('blur', function() {
+    $('.gfield input, .gfield textarea').on('blur', function() {
       const $form_group = $(this).parent().parent();
-      if ($(this).val() === '') {
+      if ($(this).val() === '' && $(this).attr('type') !== 'file') {
         $form_group.removeClass('is-focused');
       }
     });
