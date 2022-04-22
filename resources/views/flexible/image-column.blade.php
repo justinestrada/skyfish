be6802ee2bc568935e48cb9ce33a211c903d9559
Abdul Rehman <abdulrehmanali82@gmail.com>
2022-04-22 12:34:37 +0500

    On board Computing page (#23)
    * On board Computing page
    
    * remove underline text

---
+++ resources/views/flexible/image-column.blade.php
@@ -0,0 +1,15 @@
+@php
+$section_padding = get_sub_field('padding');
+$background = get_sub_field('background');
+@endphp
+<section id="flex-layout_{{ $layout_key }}" class="flex-layout flex-image-column bg-{{ $background['color'] }} {{ $section_padding }}">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        @if ($image = get_sub_field('image'))
+          <img src="{{ $image['url'] }}" alt="{{ $image['title'] }}" class="{{ $width }} mx-auto d-block"/>
+        @endif
+      </div>
+    </div>
+  </div>
+</section>
